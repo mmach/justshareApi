ad2aba404d34243f41bedac540d755ebe4fdfa73
RXS\mmach-c <mmach-c@rxsinfo.com>
2025-05-01 21:04:06 +0200

    deploy

--- src/Architecture/logConfig.js
+++ src/Architecture/logConfig.js
@@ -3,8 +3,9 @@ import path from 'path';
 import 'winston-daily-rotate-file';
 import fs from 'fs'
 import packPath from "package-json-path";
-
-var dir = packPath(path.join('logs', 'logs'))
+console.log(process.env.npm_config_local_prefix)
+console.log(packPath());
+var dir = packPath(path.join(process.env.npm_config_local_prefix,'logs', 'logs')).replace('package.json','')
 dir=dir.replace('package.json','').substring(0, dir.length-1);
 var error = packPath(path.join('logs', 'logs', 'error')).replace('package.json','');
 error=error.replace('package.json','').substring(0, error.length-1);
